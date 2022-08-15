e9adca4adc888a8b3df97fef1c54d420184fe353
Addie Morang <addiemorang@addies-mbp.myfiosgateway.com>
2022-08-14 21:40:47 -0400

    handle recurring events

--- classes/calendar.py
+++ classes/calendar.py
@@ -1,6 +1,8 @@
-from datetime import datetime
+import re
 
-from constants import GOOGLE_CALENDAR_STRPTME_FMT, GOOGLE_CALENDAR_STRPTME_FMT_DT
+from datetime import date, datetime, timedelta
+
+from constants import GOOGLE_CALENDAR_STRPTME_FMT, GOOGLE_CALENDAR_STRPTME_FMT_DT, GOOGLE_CALENDAR_WEEKDAYS
 
 
 class Calendar(object):
@@ -19,13 +21,38 @@ class Calendar(object):
             event_start = event['start']
             event_end = event['end']
             if 'dateTime' in event_start.keys() and 'dateTime' in event_end.keys():
-                start = self._to_python_datetime(event['start']['dateTime'])
-                end = self._to_python_datetime(event['end']['dateTime'])
+                event_start = event['start']['dateTime']
+                event_end = event['end']['dateTime']
                 all_day = False
             else:
-                start = self._to_python_date(event['start']['date'])
-                end = self._to_python_date(event['end']['date'])
+                event_start = event['start']['date']
+                event_end = event['end']['date']
                 all_day = True
+
+            if not all_day:
+                event_start = self._to_python_datetime(event_start)
+                event_end = self._to_python_datetime(event_end)
+            else:
+                event_start = self._to_python_date(event_start)
+                event_end = self._to_python_date(event_end)
+
+            recurring = 'recurrence' in event.keys()
+
+            if recurring:
+                recurrence_list = event['recurrence']
+                recurrence_fields = self._parse_recurrence_fields(recurrence_list)
+
+                if recurrence_fields['exception_days']:
+                    # TODO test this
+                    if event_start in recurrence_fields['exception_days']:
+                        continue
+
+                if recurrence_fields['weekday']:
+                    today = datetime.now().date()
+                    today_weekday = today.weekday()
+                    days_ahead = (recurrence_fields['weekday'] - today_weekday) % 7
+                    event_start = datetime.combine(today + timedelta(days=days_ahead), event_start.time())
+                    event_end = datetime.combine(today + timedelta(days=days_ahead), event_end.time())
 
             # configure description
             event_description = event.get('description', None)
@@ -36,8 +63,8 @@ class Calendar(object):
                 'all_day': all_day,
                 'name': event['summary'],
                 'calendar': self,
-                'start': start,
-                'end': end,
+                'start': event_start,
+                'end': event_end,
                 'event_id': event['id'],
                 'description': event_description
             }
@@ -45,6 +72,34 @@ class Calendar(object):
             new_event = Event(**event_kwargs)
             self.events.append(new_event)
 
+    def _parse_recurrence_fields(self, recurrence_list):
+        # See https://www.rfc-editor.org/rfc/rfc5545#section-3.8.5
+        exception_days = []
+        frequency = None
+        weekday = None
+
+        for recurrence_field in recurrence_list:
+            if recurrence_field.startswith('EXDATE'):
+                search_result = re.search(r'(?P<exceptions>(?<=:)[\dT,]*)', recurrence_field).group('exceptions')
+                matches = re.sub(r'T\d*', '', search_result)
+
+                for match in matches.split(','):
+                    year = int(match[:4])
+                    month = int(match[4:6])
+                    day = int(match[6:])
+                    exception_days.append(date(year, month, day))
+            elif recurrence_field.startswith('RRULE'):
+                frequency = re.search(r'(?P<frequency>(?<=FREQ=)[A-Z]*)', recurrence_field).group('frequency')
+                if frequency == 'WEEKLY':
+                    # What should we use frequency for?
+                    day = re.search(r'(?P<day>(?<=BYDAY=)[A-Z]*)', recurrence_field).group('day')
+                    weekday = GOOGLE_CALENDAR_WEEKDAYS.index(day)
+
+        return {
+            'exception_days': exception_days,
+            'frequency': frequency,
+            'weekday': weekday,
+        }
     def _to_python_datetime(self, google_calendar_datetime):
         return datetime.strptime(google_calendar_datetime[:-6], GOOGLE_CALENDAR_STRPTME_FMT_DT)
 
--- constants.py
+++ constants.py
@@ -5,3 +5,5 @@ GOOGLE_CALENDAR_API_URL = 'https://www.googleapis.com/auth/calendar'
 GOOGLE_CALENDAR_STRPTME_FMT_DT = '%Y-%m-%dT%H:%M:%S'
 
 GOOGLE_CALENDAR_STRPTME_FMT = '%Y-%m-%d'
+
+GOOGLE_CALENDAR_WEEKDAYS = ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU']
